212c42f8c17bab27bdd71c5c4bc868e5cd6c6f99
NikosGad <nikosgad96@gmail.com>
2020-01-20 23:37:20 +0200

    Add docker-compose.devel.yaml file for development

--- backend/rest_server/rest_server.py
+++ backend/rest_server/rest_server.py
@@ -16,7 +16,7 @@ def compile():
 @app.route('/', methods=['GET'])
 def show_index():
     # return render_template('./home.html')
-    return "Hello index"
+    return "<h1 style='color:red'>Hello<h1 style='color:yellow'> flask and world</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
