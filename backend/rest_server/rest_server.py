7b8559cdef1e5ff428097625810b60ff6f565396
NikosGad <nikosgad96@gmail.com>
2020-01-30 22:04:05 +0200

    Generate stdout.txt, stderr.txt and results.zip (send it in success) after compilation

--- backend/rest_server/rest_server.py
+++ backend/rest_server/rest_server.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory, make_response
 import logging
 import subprocess
+import os
 from werkzeug.utils import secure_filename
+from zipfile import ZipFile
 #from flask_cors import CORS
 
+UPLOAD_PATH_EMSCRIPTEN=os.environ.get("UPLOAD_PATH_EMSCRIPTEN", "/results/emscripten/")
 app = Flask(__name__)
 #CORS(app)
 
@@ -33,30 +36,57 @@ JSON:
 @app.route('/compile', methods=['POST'])
 def compile():
     app.logger.debug(debug_request(request))
+
     client_file = request.files["mycode"]
-    app.logger.debug("Start compilation of " + str(client_file))
-    app.logger.debug(client_file.content_type)
-    app.logger.debug(client_file.filename)
     filename = secure_filename(client_file.filename)
-    app.logger.debug(filename)
+
+    app.logger.debug("File information: " + str(client_file))
+    app.logger.debug("Secure Filename: " + filename)
+    app.logger.debug("Content-type: " + client_file.content_type)
+
     # file_content = request.files["mycode"].read()
     # app.logger.debug(file_content)
-    subprocess.run(["ls", "-la", "/results"])
-    client_file.save("/results/"+filename)
+    subprocess.run(["ls", "-la", UPLOAD_PATH_EMSCRIPTEN])
+
+    try:
+        client_file.save(UPLOAD_PATH_EMSCRIPTEN + filename)
+    except Exception:
+        app.logger.exception("An error occured while saving: {filename}". format(filename=filename))
+        return jsonify({"type": "UnexpectedException", "message": "Internal Unexpected Error"}), 500
 
     # app.logger.debug("\n" + file_content.decode("ascii"))
-    # TODO: check for mime type or make sure that it has ascii characters before compiling 
-    # compile_command = ["ls", "-la"]
-    compile_command = ["emcc", "/results/"+filename, "-o", "/results/"+filename+".html"]
+    # TODO: check for mime type or make sure that it has ascii characters before compiling
+    compile_command = ["emcc", UPLOAD_PATH_EMSCRIPTEN + filename, "-o", UPLOAD_PATH_EMSCRIPTEN + filename + ".html"]
     try:
         # DONT USE shell=True for security and vulnerabilities
-        completed_compile_file_process = subprocess.run(compile_command)
-        subprocess.run(["ls", "-la", "/results"])
+        completed_compile_file_process = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if completed_compile_file_process.stdout:
+            with open(UPLOAD_PATH_EMSCRIPTEN + "stdout.txt", "w") as f:
+                print(completed_compile_file_process.stdout.decode(encoding="utf-8"), file=f)
+
+        if completed_compile_file_process.stderr:
+            with open(UPLOAD_PATH_EMSCRIPTEN + "stderr.txt", "w") as f:
+                print(completed_compile_file_process.stderr.decode(encoding="utf-8"), file=f)
+
     except Exception:
+        subprocess.run(["ls", "-la", UPLOAD_PATH_EMSCRIPTEN])
         app.logger.exception("An error occured during: subprocess.run({compile_command})".format(compile_command=compile_command))
         return jsonify({"type": "UnexpectedException", "message": "Internal Unexpected Error"}), 500
 
-    app.logger.debug(completed_compile_file_process.returncode)
+    subprocess.run(["ls", "-la", UPLOAD_PATH_EMSCRIPTEN])
+    app.logger.debug("Compilation return code: " + str(completed_compile_file_process.returncode))
+
+    # TODO: Activate X-Sendfile
+    if completed_compile_file_process.returncode == 0:
+        with ZipFile(UPLOAD_PATH_EMSCRIPTEN + "results.zip", "w") as results_zip:
+            results_zip.write(UPLOAD_PATH_EMSCRIPTEN + filename + ".html", filename + ".html")
+            results_zip.write(UPLOAD_PATH_EMSCRIPTEN + filename + ".js", filename + ".js")
+            results_zip.write(UPLOAD_PATH_EMSCRIPTEN + filename + ".wasm", filename + ".wasm")
+
+        response = make_response(send_from_directory(UPLOAD_PATH_EMSCRIPTEN, "results.zip"), 201)
+        response.headers["Access-Control-Allow-Origin"] = "http://localhost:3535"
+        return response
     return jsonify({'message': 'OK', 'file_content': 42}), 201, {'Access-Control-Allow-Origin': 'http://localhost:3535'}
 
 ######### HTML #########
