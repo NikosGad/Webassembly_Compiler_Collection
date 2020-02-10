ff082a806c975a42d8ff01fd478b2cc6f46229ef
NikosGad <nikosgad96@gmail.com>
2020-02-10 17:05:38 +0200

    Sync backend to support C++ compilation

--- backend/rest_server/rest_server.py
+++ backend/rest_server/rest_server.py
@@ -67,6 +67,31 @@ def parse_c_compilation_options(optimization_level="", iso_standard="", suppress
 
     return compile_command, secured_output_filename
 
+def parse_cpp_compilation_options(optimization_level="", iso_standard="", suppress_warnings=False, output_filename="", **kwargs):
+    compile_command = ["em++"]
+
+    if optimization_level in ["O0", "O1", "O2", "O3", "Os", "Oz",]:
+        compile_command.append("-" + optimization_level)
+
+    if iso_standard in ["c++98", "c++03", "c++11", "c++14", "c++17", "c++2a",
+        "gnu++98", "gnu++03", "gnu++11", "gnu++14", "gnu+++17", "gnu+++2a",]:
+    # "c89", "c90", "c99", "c11", "c17", "gnu89", "gnu90", "gnu99", "gnu11", "gnu17",]:
+        compile_command.append("-std=" + iso_standard)
+
+    if suppress_warnings == True:
+        compile_command.append("-w")
+
+    secured_output_filename = secure_filename(output_filename)
+    if secured_output_filename == "":
+        secured_output_filename = "a.out"
+    else:
+        secured_output_filename = start_hyphen_sequence_pattern.sub('', secured_output_filename)
+
+    app.logger.debug("Parsed compile command: " + str(compile_command))
+    app.logger.debug("Parsed Output Filename: " + secured_output_filename)
+
+    return compile_command, secured_output_filename
+
 ############ API ############
 @app.route('/compile', methods=['POST'])
 def compile():
@@ -85,7 +110,10 @@ def compile():
         app.logger.exception("An error occured while loading compilation options json")
         return jsonify({"type": "JSONParseError", "message": "Bad JSON Format Error"}), 400
 
-    compile_command, secured_output_filename = parse_c_compilation_options(**compilation_options_json)
+    if request.form["language"] == "C":
+        compile_command, secured_output_filename = parse_c_compilation_options(**compilation_options_json)
+    elif request.form["language"] == "C++":
+        compile_command, secured_output_filename = parse_cpp_compilation_options(**compilation_options_json)
 
     subprocess.run(["ls", "-la", UPLOAD_PATH_EMSCRIPTEN])
 
