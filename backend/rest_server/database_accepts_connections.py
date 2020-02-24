3052b74974f379d2f8649450a51b86afd8632c77
NikosGad <nikosgad96@gmail.com>
2020-02-24 23:36:19 +0200

    Add a script to check that the DB accepts connection before the backend server starts

---
+++ backend/rest_server/database_accepts_connections.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect(('database-devel',5432))
+    s.close()
+    print("Connected to PostgreSQL")
+    exit(0)
+except socket.error as ex:
+    print("Connection failed with errno {0}: {1}\nWaiting for PostgreSQL...".format(ex.errno, ex.strerror))
+    exit(1)
