dda2c3e36761a07ced886557ea6a7ea74ccde633
NikosGad <nikosgad96@gmail.com>
2020-02-25 23:41:52 +0200

    Add a dummy database creation via flask and flask sqlalchemy

--- backend/rest_server/rest_server/__init__.py
+++ backend/rest_server/rest_server/__init__.py
@@ -1,4 +1,18 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DB_URL = "postgresql://{user}:{password}@ucrm_db:5432/database".format(user=POSTGRES_USER, password=POSTGRES_PASSWORD)
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 from . import rest_server
+from . import models
+
+db.create_all()
+print("DB created!")
