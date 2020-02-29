b891e31a63193bf412c025a03651af702e402dd4
NikosGad <nikosgad96@gmail.com>
2020-02-29 15:06:11 +0200

    Add bcrypt support for password hashing

--- backend/rest_server/rest_server/__init__.py
+++ backend/rest_server/rest_server/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from . import rest_server
 from . import models # Without this import create_all() will NOT create tables
