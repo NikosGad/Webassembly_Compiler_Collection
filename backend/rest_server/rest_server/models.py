720d3f9fadfbdb5a5105bc0c2ed93bb23a76451c
NikosGad <nikosgad96@gmail.com>
2020-02-29 15:11:33 +0200

    Update users model
    Password was added along with a function  that hashes the password and a function that validates the password.
    Function create and delete are also added.

--- backend/rest_server/rest_server/models.py
+++ backend/rest_server/rest_server/models.py
@@ -1,16 +1,37 @@
-from rest_server import db
+import datetime
+from rest_server import db, bcrypt
 # from sqlalchemy.dialects.postgresql import JSON
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, username, email):
+    def __init__(self, username, password, email):
         self.username = username
+        self.password = self.__hash_password(password)
         self.email = email
+        self.created_at = datetime.datetime.utcnow()
+        self.updated_at = self.created_at
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __hash_password(self, password):
+        return bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def validate_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
     def __repr__(self):
-        return '<id {}, username {}, email {}>'.format(self.id, self.username, self.email)
+        return '<id {}, username {}, password {}, email {}>'.format(self.id, self.username, self.password, self.email)
