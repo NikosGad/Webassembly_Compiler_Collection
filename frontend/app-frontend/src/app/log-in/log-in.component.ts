71f89f1e3f30d790cd6712a51bd0961690e57100
NikosGad <nikosgad96@gmail.com>
2020-03-15 20:41:05 +0200

    Implement a part of frontend log in functionality

--- frontend/app-frontend/src/app/log-in/log-in.component.ts
+++ frontend/app-frontend/src/app/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  log_in_form: FormGroup;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.log_in_form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log("Sending the form...");
+    console.log(this.log_in_form);
+    if (this.log_in_form.invalid) {
+      console.log("Invalid Log In Form!")
+      return;
+    }
+    console.log("Sent form!");
   }
 
 }
