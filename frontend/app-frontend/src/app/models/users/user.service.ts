bd6f1bc51353a7eb676c8005792acff807b48ced
NikosGad <nikosgad96@gmail.com>
2020-03-19 21:25:16 +0200

    Add user service with sign up

---
+++ frontend/app-frontend/src/app/models/users/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  public signUp(userInfo: any) {
+    const formData = new FormData();
+
+    formData.append("username", userInfo.username);
+    formData.append("email", userInfo.email);
+    formData.append("password", userInfo.password);
+
+    return this.http.post('http://127.0.0.1:8080/api/signup', formData);
+  }
+}
