cf861751fa7be8e7382c33bb4c3acc34b84da0a4
NikosGad <nikosgad96@gmail.com>
2020-04-02 19:39:07 +0300

    Append Authentication Service with logout(), isLoggedIn() and localStorage for jwt

--- frontend/app-frontend/src/app/authentication.service.ts
+++ frontend/app-frontend/src/app/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,25 @@ export class AuthenticationService {
     formData.append("username", userInfo.username);
     formData.append("password", userInfo.password);
 
-    return this.http.post('http://127.0.0.1:8080/api/login', formData);
+    return this.http.post('http://127.0.0.1:8080/api/login', formData).pipe(
+      map((response:any) => {
+        console.log("Login Request Response:", response);
+        console.log("Local Storage Before Login:", localStorage);
+        localStorage.setItem("jwt", response.jwt);
+        console.log("Local Storage After  Login:", localStorage);
+        return response.message;
+      })
+    );
+  }
+
+  logout() {
+    console.log("Local Storage Before Logout: ", localStorage);
+    localStorage.removeItem("jwt");
+    console.log("Local Storage After  Logout: ", localStorage);
+  }
+
+  isLoggedIn() {
+     /* If jwt is not null then return true (a user is logged in) */
+     return localStorage.getItem("jwt") != null
   }
 }
