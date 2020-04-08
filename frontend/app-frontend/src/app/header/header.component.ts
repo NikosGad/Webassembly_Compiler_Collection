917d0e664638022f603397d5354a52fa053d91b6
NikosGad <nikosgad96@gmail.com>
2020-04-08 22:37:23 +0300

    Add ngOnDestroy() in HeaderComponent to unsubscribe and free resources

--- frontend/app-frontend/src/app/header/header.component.ts
+++ frontend/app-frontend/src/app/header/header.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isLoggedIn: boolean;
+  isLoggedInSubscription: Subscription;
 
   constructor(private authenticationService: AuthenticationService, private router: Router) {
-    this.authenticationService.isLoggedInObs.subscribe(login_state => this.isLoggedIn = login_state);
+    this.isLoggedInSubscription = this.authenticationService.isLoggedInObs.subscribe(login_state => this.isLoggedIn = login_state);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    if (this.isLoggedInSubscription != null) {
+      this.isLoggedInSubscription.unsubscribe();
+    }
   }
 
   logout() {
