dcdd33c258f2583497ca089e82d163d2da27bc58
NikosGad <nikosgad96@gmail.com>
2020-04-12 20:51:02 +0300

    Frontend: Create a nice file details card

--- frontend/app-frontend/src/app/personal-file-details/personal-file-details.component.ts
+++ frontend/app-frontend/src/app/personal-file-details/personal-file-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-personal-file-details',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-file-details.component.css']
 })
 export class PersonalFileDetailsComponent implements OnInit {
+  @Input() file: any;
+  // file_content: string;
+  // { name, path, language, options, status, content },
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  getFileSourceCode() {
+    this.http.get('http://127.0.0.1:8080/api/personal_files', { responseType: 'text'}).subscribe(
+      (res:any) => {
+        console.log(res);
+        this.file.content = res;
+        console.log(this.file.content);
+      }
+    );
   }
 
 }
