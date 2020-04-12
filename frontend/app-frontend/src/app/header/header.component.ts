ddfc77124ca96f445ec0b7f5ddda53bb1f20b8d7
NikosGad <nikosgad96@gmail.com>
2020-04-12 20:47:05 +0300

    Toolbar is elevated and navigation to personal files is available

--- frontend/app-frontend/src/app/header/header.component.ts
+++ frontend/app-frontend/src/app/header/header.component.ts
@@ -27,7 +27,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   logout() {
     this.authenticationService.logout();
-    this.router.navigate(["login"])
+    this.router.navigate(["login"]);
   }
 
 }
