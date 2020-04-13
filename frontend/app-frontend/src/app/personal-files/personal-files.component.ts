65eae40c60b24805a7b40cf9cddb951344ab744e
NikosGad <nikosgad96@gmail.com>
2020-04-13 23:03:42 +0300

    Frontend: Create a nice list to show the files

--- frontend/app-frontend/src/app/personal-files/personal-files.component.ts
+++ frontend/app-frontend/src/app/personal-files/personal-files.component.ts
@@ -6,17 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-files.component.css']
 })
 export class PersonalFilesComponent implements OnInit {
-  files = [
-    { name: "hello.c", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C", options: "-O2, gnu11", status:"Successful", content: "" },
-    { name: "hello.go", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "Golang", options: "-O2", status: "Erroneous", content: "" },
-    { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
-  ];
+  files = {
+      "C": [
+          { name: "hello.c", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C", options: "-O2, gnu11", status:"Successful", content: "" },
+      ],
+      "C++": [
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+          { name: "hello.cpp", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "C++", options: "-O2, gnu++11", status:"Successful", content: "" },
+      ],
+      "Golang": [
+          { name: "hello.go", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "Golang", options: "-O2", status: "Erroneous", content: "" },
+          { name: "hello.go", path: "/results/emscripten/2020-04-09 20:36:12.116313_f1bcfccb06e22196a782b8e74107dff168d0215224ed1b1116179e8717ed358e/", language: "Golang", options: "-O2", status: "Erroneous", content: "" },
+      ],
+      "Rust": [],
+  };
 
-  selected_file = this.files[0];
+  available_languages = ["C", "C++", "Golang", "Rust",]
+
+  selected_file: any;
+  all_files_list: any;
+  filtered_files: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.selected_file = "";
+    this.all_files_list = [];
+
+    for (let language of this.available_languages) {
+      console.log(language);
+      console.log(this.files[language]);
+      this.all_files_list = this.all_files_list.concat(this.files[language]);
+    }
+
+    this.filtered_files = this.all_files_list;
+    console.log(this.all_files_list);
   }
 
   onFileSelect(file:any): void {
@@ -26,6 +80,19 @@ export class PersonalFilesComponent implements OnInit {
 
   closeDetails() {
     this.selected_file = null;
+  }
+
+  filterFiles(language: string) {
+    console.log(language);
+
+    if (language) {
+      this.filtered_files = this.files[language];
+    }
+    else {
+      this.filtered_files = this.all_files_list;
+    }
+
+    console.log(this.filtered_files);
   }
 
 }
