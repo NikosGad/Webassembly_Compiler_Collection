4a1f832d4c5c86b3c9f736c4f6439533e3c4d4f0
NikosGad <nikosgad96@gmail.com>
2020-05-08 19:40:18 +0300

    System: Add a simple argument in order to delete branch images

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,53 @@
 #!/usr/bin/env bash
 
-if [[ -d /results ]]
-then
-    echo "/results exists on your filesystem."
-    echo "Please move its contents to another directory and remove the directory, so that the deployment can continue."
-    echo "Exiting..."
-    exit 1
-fi
+function usage() {
+    echo "deploy.sh [--delete-images branch] [--help|-h]"
+}
 
-docker-compose up
+while [[ "$1" != "" ]]; do
+    case "$1" in
+        --delete-images )
+            shift
+            if [[ -n $1 ]] && [[ $1 != -* ]]; then
+                echo "Images:"
+                docker images --filter=reference="*:$1"
 
-exit 0
+                answer=""
+                while [[ ${answer} != "yes" && ${answer} != "y" && ${answer} != "no" && ${answer} != "n" ]]; do
+                    printf "Are you sure you want to delete these images?(yes/no) "
+                    read answer
+                done
+
+                if [[ ${answer} = "yes" || ${answer} = "y" ]]; then
+                    echo "Deleting images..."
+                    docker rmi $(docker images --filter=reference="*:$1" --format="{{.Repository}}:{{.Tag}}")
+                    echo "Images Deleted"
+                fi
+
+                shift
+            else
+                usage
+                exit 1
+            fi
+            ;;
+        --help | -h )
+            usage
+            exit 0
+            ;;
+        * )
+            usage
+            exit 1
+            ;;
+    esac
+done
+# if [[ -d /results ]]
+# then
+#     echo "/results exists on your filesystem."
+#     echo "Please move its contents to another directory and remove the directory, so that the deployment can continue."
+#     echo "Exiting..."
+#     exit 1
+# fi
+#
+# docker-compose up
+#
+# exit 0
