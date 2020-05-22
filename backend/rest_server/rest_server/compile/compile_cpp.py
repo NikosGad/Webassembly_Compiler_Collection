1624d56af85dd70bf3ee8804d467898b6a7e409d
NikosGad <nikosgad96@gmail.com>
2020-05-22 19:21:06 +0300

    Backend: Fix compile_cpp.py based on test_compile_cpp.py discovered failures

--- backend/rest_server/rest_server/compile/compile_cpp.py
+++ backend/rest_server/rest_server/compile/compile_cpp.py
@@ -20,28 +20,27 @@ class CppCompilationHandler(CompilationHandler):
             parsed_compilation_options.append("-" + optimization_level)
 
         if iso_standard in ["c++98", "c++03", "c++11", "c++14", "c++17", "c++2a",
-            "gnu++98", "gnu++03", "gnu++11", "gnu++14", "gnu+++17", "gnu+++2a",]:
+            "gnu++98", "gnu++03", "gnu++11", "gnu++14", "gnu++17", "gnu++2a",]:
             parsed_compilation_options.append("-std=" + iso_standard)
 
         if suppress_warnings == True:
             parsed_compilation_options.append("-w")
 
         secured_output_filename = secure_filename(output_filename)
+        secured_output_filename = re.compile('^-+').sub('', secured_output_filename)
         if secured_output_filename == "":
             secured_output_filename = "a.out"
-        else:
-            secured_output_filename = re.compile('^-+').sub('', secured_output_filename)
 
         return parsed_compilation_options, secured_output_filename
 
     def compilation_command_generator(self, working_directory, parsed_compilation_options, input_filename, output_filename):
         compilation_command = ["em++"]
         compilation_command.extend(parsed_compilation_options)
-        compilation_command.extend(["-o", working_directory + output_filename + ".html", working_directory + input_filename])
+        compilation_command.extend(["-o", working_directory + "/" + output_filename + ".html", working_directory + "/" + input_filename])
         return compilation_command
 
-    def results_zip_appender(self, working_directory, results_zip_name, output_filename, mode, compression, compresslevel):
-        with ZipFile(file=working_directory + results_zip_name, mode=mode, compression=compression, compresslevel=compresslevel) as results_zip:
-            results_zip.write(working_directory + output_filename + ".html", output_filename + ".html")
-            results_zip.write(working_directory + output_filename + ".js", output_filename + ".js")
-            results_zip.write(working_directory + output_filename + ".wasm", output_filename + ".wasm")
+    def results_zip_appender(self, working_directory, results_zip_name, output_filename, compression, compresslevel):
+        with ZipFile(file=working_directory + "/" + results_zip_name, mode="a", compression=compression, compresslevel=compresslevel) as results_zip:
+            results_zip.write(working_directory + "/" + output_filename + ".html", output_filename + ".html")
+            results_zip.write(working_directory + "/" + output_filename + ".js", output_filename + ".js")
+            results_zip.write(working_directory + "/" + output_filename + ".wasm", output_filename + ".wasm")
