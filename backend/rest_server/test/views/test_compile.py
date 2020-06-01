4f526ccba175c4747f42ed808dfa6d0a2157eaf7
NikosGad <nikosgad96@gmail.com>
2020-06-01 20:52:38 +0300

    Backend: Add some UTs and adjust existing UTs for module views/compile.py (remove FT logic) (100% coverage)

--- backend/rest_server/test/views/test_compile.py
+++ backend/rest_server/test/views/test_compile.py
@@ -6,7 +6,7 @@ import unittest
 from zipfile import ZipFile
 
 from rest_server import app, authentication
-from rest_server.models import user_model
+from rest_server.models import user_model, file_model
 from rest_server.compile import compilation_handlers_dictionary
 
 class ViewCompileTestCase(unittest.TestCase):
@@ -41,6 +41,10 @@ class ViewCompileTestCase(unittest.TestCase):
             else:
                 os.remove(entry_path)
 
+        test_all_files = file_model.SourceCodeFile.get_all_files()
+        for file in test_all_files:
+            file.delete()
+
     def setUp(self):
         root_upload_path_list = os.listdir(self.handler_c.root_upload_path)
 
@@ -52,6 +56,10 @@ class ViewCompileTestCase(unittest.TestCase):
                 )
             )
 
+        test_all_files = file_model.SourceCodeFile.get_all_files()
+        if test_all_files != []:
+            raise Exception("There are leftovers in source_code_files table in DB")
+
     def tearDown(self):
         for entry in os.listdir(self.handler_c.root_upload_path):
             entry_path = self.handler_c.root_upload_path + "/" + entry
@@ -59,6 +67,10 @@ class ViewCompileTestCase(unittest.TestCase):
                 shutil.rmtree(entry_path)
             else:
                 os.remove(entry_path)
+
+        test_all_files = file_model.SourceCodeFile.get_all_files()
+        for file in test_all_files:
+            file.delete()
 
     def test_compile_store__unauthorized(self):
         mock_request = {
@@ -75,64 +87,31 @@ class ViewCompileTestCase(unittest.TestCase):
         self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
         self.assertEqual(response.get_json(), {"type": "AuthorizationViolation", "message": "Authorization Header is missing"})
 
-    def test_compile__invalid_language(self):
-        mock_request = {
-            "base_url": "http://127.0.0.1:8080",
-            "path": "/api/compile/invalid-language",
-        }
-
-        with app.test_client() as c:
-            response = c.post(**mock_request)
-
-        self.assertEqual(response._status, "400 BAD REQUEST")
-        self.assertEqual(response.headers.get("Content-Type"), "application/json")
-        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
-        self.assertEqual(response.get_json(), {"type": "LanguageNotSupportedError", "message": "Language invalid-language is not supported."})
-
-    def test_compile_store__invalid_language(self):
-        mock_request = {
-            "base_url": "http://127.0.0.1:8080",
-            "path": "/api/compile/invalid-language/store",
-            "headers": {
-                "Authorization": "Bearer " + self.token_valid
-            },
-        }
-
-        with app.test_client() as c:
-            response = c.post(**mock_request)
-
-        self.assertEqual(response._status, "400 BAD REQUEST")
-        self.assertEqual(response.headers.get("Content-Type"), "application/json")
-        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
-        self.assertEqual(response.get_json(), {"type": "LanguageNotSupportedError", "message": "Language invalid-language is not supported."})
-
-    def test_compile_C__missing_query_parameters(self):
-        data_list = [
-            {},
+    def test_compile_and_compile_store__invalid_language(self):
+        mock_request_list = [
             {
-                "code": (self.c_source_code_snippet_hello_c, None),
+                "base_url": "http://127.0.0.1:8080",
+                "path": "/api/compile/invalid-language",
             },
             {
-                "compilation_options": '{"optimization_level": "O2", "iso_standard": "gnu11", "suppress_warnings": true, "output_filename": "-----hello"}',
-            },
+                "base_url": "http://127.0.0.1:8080",
+                "path": "/api/compile/invalid-language/store",
+                "headers": {
+                    "Authorization": "Bearer " + self.token_valid
+                },
+            }
         ]
 
-        for data in data_list:
-            mock_request = {
-                "base_url": "http://127.0.0.1:8080",
-                "path": "/api/compile/C",
-                "data": data,
-            }
-
+        for mock_request in mock_request_list:
             with app.test_client() as c:
                 response = c.post(**mock_request)
 
             self.assertEqual(response._status, "400 BAD REQUEST")
             self.assertEqual(response.headers.get("Content-Type"), "application/json")
             self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
-            self.assertEqual(response.get_json(), {"type": "IncorrectCompileBodyError", "message": "A form data should be provided that contains a file with key 'code' and a compilation options json with key 'compilation_options'."})
+            self.assertEqual(response.get_json(), {"type": "LanguageNotSupportedError", "message": "Language invalid-language is not supported."})
 
-    def test_compile_store_C__missing_query_parameters(self):
+    def test_compile_C_and_compile_store_C__missing_query_parameters(self):
         data_list = [
             {},
             {
@@ -143,23 +122,31 @@ class ViewCompileTestCase(unittest.TestCase):
             },
         ]
 
-        for data in data_list:
-            mock_request = {
+        mock_request_list = [
+            {
+                "base_url": "http://127.0.0.1:8080",
+                "path": "/api/compile/C",
+            },
+            {
                 "base_url": "http://127.0.0.1:8080",
                 "path": "/api/compile/C/store",
                 "headers": {
                     "Authorization": "Bearer " + self.token_valid
                 },
-                "data": data,
             }
+        ]
 
-            with app.test_client() as c:
-                response = c.post(**mock_request)
+        for data in data_list:
+            for mock_request in mock_request_list:
+                mock_request["data"] = data
 
-            self.assertEqual(response._status, "400 BAD REQUEST")
-            self.assertEqual(response.headers.get("Content-Type"), "application/json")
-            self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
-            self.assertEqual(response.get_json(), {"type": "IncorrectCompileBodyError", "message": "A form data should be provided that contains a file with key 'code' and a compilation options json with key 'compilation_options'."})
+                with app.test_client() as c:
+                    response = c.post(**mock_request)
+
+                self.assertEqual(response._status, "400 BAD REQUEST")
+                self.assertEqual(response.headers.get("Content-Type"), "application/json")
+                self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
+                self.assertEqual(response.get_json(), {"type": "IncorrectCompileBodyError", "message": "A form data should be provided that contains a file with key 'code' and a compilation options json with key 'compilation_options'."})
 
     def test_compile_C__valid(self):
         mock_request = {
@@ -238,85 +225,90 @@ class ViewCompileTestCase(unittest.TestCase):
                         msg="WASM files do not have the same size inside the two zip files"
                     )
 
-    def test_compile_C__invalid_compilation_options_json(self):
+    def test_compile_store_C__valid(self):
         mock_request = {
             "base_url": "http://127.0.0.1:8080",
-            "path": "/api/compile/C",
+            "path": "/api/compile/C/store",
+            "headers": {
+                "Authorization": "Bearer " + self.token_valid
+            },
             "data": {
                 "code": (self.c_source_code_snippet_hello_c, "hello.c"),
-                "compilation_options": '{"optimization_level": "O2", "iso_standard": "gnu11, "suppress_warnings": true, "output_filename": "-----hello"}',
+                "compilation_options": '{"optimization_level": "O2", "iso_standard": "gnu11", "suppress_warnings": true, "output_filename": "-----hello"}',
             },
         }
 
         with app.test_client() as c:
             response = c.post(**mock_request)
 
-        self.assertEqual(response._status, "400 BAD REQUEST")
-        self.assertEqual(response.headers.get("Content-Type"), "application/json")
-        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
-        self.assertEqual(response.get_json(), {"type": "JSONParseError", "message": "Bad JSON Format Error"})
-
-    def test_compile_C__erroneous_code(self):
-        mock_request = {
-            "base_url": "http://127.0.0.1:8080",
-            "path": "/api/compile/C",
-            "data": {
-                "code": (self.c_source_code_snippet_hello_error_c, "hello_error.c"),
-                "compilation_options": '{"optimization_level": "O2", "suppress_warnings": true, "iso_standard": "gnu11", "output_filename": "-----hello"}',
-            },
-        }
-
-        with app.test_client() as c:
-            response = c.post(**mock_request)
-
-        self.assertEqual(response._status, "400 BAD REQUEST")
+        self.assertEqual(response._status, "200 OK")
         self.assertEqual(response.headers.get("Content-Type"), "application/zip")
         self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "http://localhost:3535")
         self.assertEqual(response.headers.get("Content-Disposition"), "attachment; filename=results.zip")
 
         root_upload_path_list = os.listdir(self.handler_c.root_upload_path)
-        self.assertEqual(root_upload_path_list, ["unknown"],
-            msg="Path {path} does not contain only the unknown folder: {root_upload_path_list}".format(
+        self.assertEqual(root_upload_path_list, [str(self.user_info["id"])],
+            msg="Path {path} does not contain only the '{user_id}' folder: {root_upload_path_list}".format(
                 path=self.handler_c.root_upload_path,
+                user_id=self.user_info["id"],
                 root_upload_path_list=root_upload_path_list
             )
         )
 
-        unknown_directory = self.handler_c.root_upload_path + "/unknown"
-        unknown_directory_list = os.listdir(unknown_directory)
-        self.assertEqual(len(unknown_directory_list), 1,
+        user_directory = self.handler_c.root_upload_path + "/" + str(self.user_info["id"])
+        user_directory_list = os.listdir(user_directory)
+        self.assertEqual(len(user_directory_list), 1,
             msg="Path {path} does not contain exactly one uploaded file directory: {entry_list}".format(
-                path=unknown_directory,
-                entry_list=unknown_directory_list
+                path=user_directory,
+                entry_list=user_directory_list
             )
         )
 
-        uploaded_file_directory = unknown_directory + "/" + unknown_directory_list[0]
+        uploaded_file_directory = user_directory + "/" + user_directory_list[0]
         uploaded_file_directory_list = os.listdir(uploaded_file_directory)
-        self.assertEqual(len(uploaded_file_directory_list), 3,
+        self.assertEqual(len(uploaded_file_directory_list), 5,
             msg="Path {path} does not contain exactly 5 files: {entry_list}".format(
                 path=uploaded_file_directory,
                 entry_list=uploaded_file_directory_list
             )
         )
-        self.assertIn("hello_error.c", uploaded_file_directory_list)
-        self.assertIn("stderr.txt", uploaded_file_directory_list)
+        self.assertIn("hello.c", uploaded_file_directory_list)
+        self.assertIn("hello.html", uploaded_file_directory_list)
+        self.assertIn("hello.js", uploaded_file_directory_list)
+        self.assertIn("hello.wasm", uploaded_file_directory_list)
         self.assertIn("results.zip", uploaded_file_directory_list)
-        self.assertTrue(filecmp.cmp(uploaded_file_directory + "/hello_error.c", self.c_source_code_snippet_hello_error_c))
-        self.assertTrue(os.stat(uploaded_file_directory + "/stderr.txt").st_size != 0)
+        self.assertTrue(filecmp.cmp(uploaded_file_directory + "/hello.c", self.c_source_code_snippet_hello_c))
+        self.assertTrue(os.stat(uploaded_file_directory + "/hello.html").st_size != 0)
+        self.assertTrue(os.stat(uploaded_file_directory + "/hello.js").st_size != 0)
+        self.assertTrue(os.stat(uploaded_file_directory + "/hello.wasm").st_size != 0)
 
         with ZipFile(file=uploaded_file_directory + "/results.zip", mode="r") as file_system_zip:
             self.assertIsNone(file_system_zip.testzip())
-            self.assertEqual(file_system_zip.namelist(), ["stderr.txt"])
+            self.assertEqual(file_system_zip.namelist(), ["hello.html", "hello.js", "hello.wasm"])
             file_system_zip_infolist = file_system_zip.infolist()
-            self.assertEqual(os.stat(uploaded_file_directory + "/stderr.txt").st_size, file_system_zip_infolist[0].file_size)
+            self.assertEqual(os.stat(uploaded_file_directory + "/hello.html").st_size, file_system_zip_infolist[0].file_size)
+            self.assertEqual(os.stat(uploaded_file_directory + "/hello.js").st_size, file_system_zip_infolist[1].file_size)
+            self.assertEqual(os.stat(uploaded_file_directory + "/hello.wasm").st_size, file_system_zip_infolist[2].file_size)
 
             with io.BytesIO(response.get_data()) as in_memory_response_zip:
                 with ZipFile(in_memory_response_zip, 'r') as response_zip:
                     self.assertIsNone(response_zip.testzip())
-                    self.assertEqual(response_zip.namelist(), ["stderr.txt"])
+                    self.assertEqual(response_zip.namelist(), ["hello.html", "hello.js", "hello.wasm"])
                     response_zip_infolist = response_zip.infolist()
 
                     self.assertEqual(file_system_zip_infolist[0].file_size, response_zip_infolist[0].file_size,
-                        msg="STDERR files do not have the same size inside the two zip files"
+                        msg="HTML files do not have the same size inside the two zip files"
                     )
+                    self.assertEqual(file_system_zip_infolist[1].file_size, response_zip_infolist[1].file_size,
+                        msg="JS files do not have the same size inside the two zip files"
+                    )
+                    self.assertEqual(file_system_zip_infolist[2].file_size, response_zip_infolist[2].file_size,
+                        msg="WASM files do not have the same size inside the two zip files"
+                    )
+
+        test_all_files = file_model.SourceCodeFile.get_all_files()
+        self.assertEqual(len(test_all_files), 1,
+            msg="There is not exactly 1 file in DB: {files}".format(
+                files=test_all_files
+            )
+        )
