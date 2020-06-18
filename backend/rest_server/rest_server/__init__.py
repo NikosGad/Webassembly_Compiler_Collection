bd7e33b07afa4356217ffef2e0fbb1d577b06a63
NikosGad <nikosgad96@gmail.com>
2020-06-18 20:40:16 +0300

    Backend: Replace hardcoded localhost ip with the HOST_FRONTEND_ADDRESS env variable in rest_server backend application

--- backend/rest_server/rest_server/__init__.py
+++ backend/rest_server/rest_server/__init__.py
@@ -4,14 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+SYSTEM_IP = os.environ["SYSTEM_IP"]
+HOST_FRONTEND_ADDRESS = os.environ["HOST_FRONTEND_ADDRESS"]
 DB_URL = "postgresql://{user}:{password}@ucrm_db:5432/database".format(user=POSTGRES_USER, password=POSTGRES_PASSWORD)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app, origins="http://localhost:3535")
+CORS(app, origins="http://" + HOST_FRONTEND_ADDRESS)
 db = SQLAlchemy(app)
 bcr = Bcrypt(app)
 
